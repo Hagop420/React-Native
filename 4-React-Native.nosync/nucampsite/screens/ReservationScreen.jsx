1127ca4b0e78a4b98adee8768970d31ae8591966
Hagop420 <jacka161swe@gmail.com>
2023-02-27 22:55:21 -0800

    adding reservation form in RN

--- 4-React-Native.nosync/nucampsite/screens/ReservationScreen.jsx
+++ 4-React-Native.nosync/nucampsite/screens/ReservationScreen.jsx
@@ -122,11 +122,24 @@ const ReservationScreen = () => {
       )
    }
 
-   return (
-      <>
 
-      </>
-   )
 }
+
+const styles = StyleSheet.create({
+    formRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    formLabel: {
+        fontSize: 18,
+        flex: 2
+    },
+    formItem: {
+        flex: 1
+    }
+});
 
 export default ReservationScreen
