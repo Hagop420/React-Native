95915c57a036634b4c8333dc5e7e6c05af9cceef
Hagop420 <jacka161swe@gmail.com>
2023-03-01 17:41:31 -0800

    week 3 animations

---
+++ 4-React-Native.nosync/nucampsite/screens/FavoritesScreen.jsx
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import { View, FlatList, Text } from 'react-native';
+import { Avatar, ListItem } from 'react-native-elements';
+import Loading from '../components/LoadingComponent'
+import { baseUrl } from "../shared/baseUrl";
+
+
+
+const FavoritesScreen = ({ navigation }) => {
+   
+}
+
+
+export default FavoritesScreen
