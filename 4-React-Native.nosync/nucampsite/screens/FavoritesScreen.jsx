85654bba7c17b1a505c2a172f919dee4c490388d
Hagop420 <jacka161swe@gmail.com>
2023-03-01 19:12:41 -0800

    week 3 animations

--- 4-React-Native.nosync/nucampsite/screens/FavoritesScreen.jsx
+++ 4-React-Native.nosync/nucampsite/screens/FavoritesScreen.jsx
@@ -7,7 +7,27 @@ import { baseUrl } from "../shared/baseUrl";
 
 
 const FavoritesScreen = ({ navigation }) => {
+   const {campsitesArray, isLoding, errMess} = useSelector((state) => state.campsites)
+   const favorites = useSelector((state) => state.favorites)
+
+
+   if (isLoding) {
+    return <Loading/>
+   }
    
+   // error
+   if (errMess) {
+      return (
+         <View>
+            <Text>{errMess}</Text>
+         </View>
+    )
+   }
+   return (
+      <FlatList
+         data={campsitesArray.filter((campsite) => favorites.includes(campsite.id))}
+         renderItem={renderFavItem} />
+   )
 }
 
 
