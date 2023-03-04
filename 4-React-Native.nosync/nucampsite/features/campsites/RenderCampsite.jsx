29e93bbacfcd185272b36bba9c70df9a0da3a1a1
Hagop420 <jacka161swe@gmail.com>
2023-03-04 13:04:10 -0800

    modal week 3 images

--- 4-React-Native.nosync/nucampsite/features/campsites/RenderCampsite.jsx
+++ 4-React-Native.nosync/nucampsite/features/campsites/RenderCampsite.jsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Alert, PanResponder, Text, View, Button } from "react-native";
-import { Card, Icon } from "react-native-elements";
+import { StyleSheet, Alert, Modal, PanResponder, TouchableOpacity, Text, View, Button } from "react-native";
+import { Card, Input, Icon, Rating } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 import * as Animatable from 'react-native-animatable'
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 
 
 const RenderCampsite = (props) => {
-
+   const [rating, setRating] = useState(5)
+   const [author, setAuthor] = useState('')
+   const [text, setText] = useState('')
 
    const {campsite} = props
+   const [showModal, setShowModal] = useState(false)
    const isLeftSwipe = ({dx}) => dx < -200
    const view=useRef()
    const isRightSwipe = ({dx}) => dx < 200
@@ -25,37 +30,44 @@ const RenderCampsite = (props) => {
       onPanResponderEnd: (e, gState) => {
          console.log('Pan responder end:', gState);
 
-        
-         
          if (isLeftSwipe(gState)) {
-            Alert.alert(
-               'Add to Favorites',
-               'Are you sure you wish to add ' +
-               campsite.name +
-               ' to favorites?',
-               [
-                  {
-                     text: 'Cancel',
-                     style: 'cancel',
-                     onPress:()=> console.log('Cancel Pressed')
-                  },
-                  {
-                     text: 'Ok',
-                     onPress:()=> props.isFavorite ? console.log('Already set as Favorite') : props.markFavorite()
-                  }
-                  
-               ],
-               {cancelable:false}
-            )
+            handleSubmit();
+            setShowModal(true)
          }
       }
    })
+
+   const resetForm = () => {
+      setRating(5)
+      setAuthor(5)
+      setText('')
+      setShowModal(!showModal)
+   }
+
+
+   const handleSubmit = () => {
+      const newComment = {
+        author,
+        rating,
+        text,
+        campsiteId: campsite.id
+      };
+ 
+     setShowModal(!showModal)
+  }
+   
+   const toggleModal = () => {
+      setShowModal(!showModal)
+   }
+   // const dispatch=useDispatch()
+
    if (campsite) {
+
       return (
          <Animatable.View
-            animation={'fadeInDownBig'}
+            // animation={'fadeInDownBig'}
             duration={900}
-         ref={view}
+            ref={view}
             {...PanHandlerResponder.panHandlers}
             delay={900}>
          <Card containerStyle={ styles.cardContain }>
@@ -92,6 +104,95 @@ const RenderCampsite = (props) => {
             {/* pencil icon end */}
             
         </Card>
+
+        <Modal
+            animationType='fade'
+            transparent={false}
+            visible={showModal}
+            onShowModal={() => setShowModal(!showModal)}
+         
+         >
+            <View style={styles.modal}>
+               <Rating
+                  showRating
+                  imageSize={40}
+                  readonly
+                  onFinishRating={(rating) => setRating(rating)} 
+                  startingValue={1}
+                  style={{ paddingVertical: 10 }}
+               
+               />
+
+               <Input
+                  placeholder='Author'
+                  leftIconContainerStyle={{paddingRight:10}}
+                  
+                  onChangeText={(rating) => setRating(rating)}
+                  leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                  
+                  
+               />
+                  
+               
+               
+               <Input
+                placeholder='Comment'
+                leftIcon={{type: 'font-awesome',name:'comment-o'}}
+                leftIconContainerStyle={{paddingRight:10}}
+                onChangeText={(rating) => setRating(rating)}
+               />
+               
+               <View
+                  style={{margin:10}}>
+                
+
+                  {/* touch submit */}
+                  <TouchableOpacity
+                      onPress={() => {
+                        handleSubmit();
+                        resetForm();
+                      }} >
+               
+                     <Text
+                        style=
+                        {{
+                           textAlign: 'center',
+                           padding: 10,
+                           margin:10,
+                           color: '#fff',
+                           fontWeight: 'bold',
+                           backgroundColor:'blue'}}
+                        onPress={() => {
+                           handleSubmit();
+                           resetForm();
+                        }}
+                      >Submit</Text>
+  
+                  </TouchableOpacity>
+                  {/* end */}
+
+               </View>
+                  
+                  
+                  
+               <View style={{margin: 20}}>
+                  <TouchableOpacity
+                     onPress={() => setShowModal(!showModal)} >
+               
+                     <Text
+                        onPress={() => {
+                           // handleSubmit();
+                           resetForm();
+                        }}
+                        style={styles.cancelButton}>Cancel</Text>
+  
+                  </TouchableOpacity>
+               </View>
+            </View>
+         </Modal>
+         
+       
+        
         </Animatable.View>
       )
       return <View/>
@@ -100,25 +201,76 @@ const RenderCampsite = (props) => {
 
 const styles = StyleSheet.create({
    cardContain: {
-      padding: 0,
-      margin: 0,
-      marginBottom: 20,
+     padding: 0,
+     margin: 0,
+     marginBottom: 20,
    },
-   cardRow:{
-   alignItems: 'center',
-   justifyContent: 'center',
-   flex: 1,
-   flexDirection: 'row',
-   margin: 20,
-},
+   cardRow: {
+     alignItems: "center",
+     justifyContent: "center",
+     flex: 1,
+     flexDirection: "row",
+     margin: 20,
+   },
    cardText: {
-      textShadowColor: 'rgba(0, 0, 0, 1)',
-      textShadowOffset: { width: -1, height: 1 },
-      textShadowRadius: 20,
-      textAlign: 'center',
-      color: 'white',
-      fontSize: 20
-   }
-})
-
-export default RenderCampsite
+     textShadowColor: "rgba(0, 0, 0, 1)",
+     textShadowOffset: { width: -1, height: 1 },
+     textShadowRadius: 20,
+     textAlign: "center",
+     color: "white",
+     fontSize: 20,
+   },
+   modal: {
+     justifyContent: "center",
+     margin: 20,
+   },
+   modalTitle: {
+     fontSize: 24,
+     fontWeight: "bold",
+     backgroundColor: "#5637DD",
+     textAlign: "center",
+     color: "#fff",
+     marginBottom: 20,
+   },
+   modalText: {
+     fontSize: 18,
+     margin: 10,
+   },
+   modalButtons: {
+     margin: 10,
+   },
+ });
+ 
+//  const styles = StyleSheet.create({
+//    formRow: {
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//       flex: 1,
+//       flexDirection: 'row',
+//       margin: 20
+//    },
+//    formLabel: {
+//       fontSize: 18,
+//       flex: 2
+//    },
+//    formItem: {
+//       flex: 1
+//    },
+//    modal: {
+//       justifyContent: 'center',
+//       margin: 20,
+//    },
+//    modalTitle: {
+//       fontSize: 24,
+//       fontWeight: 'bold',
+//       backgroundColor: 'blue',
+//       textAlign: 'center',
+//       marginBottom: 20,
+//       color:'#fff',
+//    },
+//    modalText: {
+//       fontSize: 18,
+//       margin: 10
+//   }
+// });
+ export default RenderCampsite
