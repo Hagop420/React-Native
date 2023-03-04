fe6ed5e027cdb3b50a396105fc6ef1c85e5ea8b9
Hagop420 <jacka161swe@gmail.com>
2023-03-03 19:08:33 -0800

    Persist Redux Store

--- 4-React-Native.nosync/nucampsite/App.js
+++ 4-React-Native.nosync/nucampsite/App.js
@@ -1,7 +1,9 @@
 import Main from "./screens/MainComponent";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from './components/LoadingComponent';
 
 
 
@@ -9,9 +11,14 @@ export default function App() {
   return (
   
       <Provider store={store}>
+      <PersistGate
+        loading={<Loading />}
+        persistor={persistor} >
+      
         <NavigationContainer>
           <Main/>
         </NavigationContainer>
+        </PersistGate>
       </Provider>
     
     )
