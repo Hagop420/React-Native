52935876429aef4ff459fd68ff0703062b6408b9
Hagop420 <jacka161swe@gmail.com>
2023-03-08 17:50:14 -0800

    new login form component

---
+++ 4-React-Native.nosync/nucampsite/screens/LoginScreen.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import { CheckBox, Input } from 'react-native-elements';
+import * as SecureStore from 'expo-secure-store';
+
+
+
+const LoginScreen = () => {
+   const [userName, setUserName] = useState('')
+   const [password, setPassword] = useState('')
+   const [rememberMe, setRememberMe] = useState(false)
+
+
+
+   const handleLogin = () => {
+      console.log(`username: ${userName}`);
+      console.log(`password: ${password}`);
+      console.log(`Remember_Me: ${rememberMe}`);
+   }
+
+   return (
+      <View style={StyleSheet.container}>
+         <Input
+            placeholder='username'
+            leftIcon={{ type: 'font-awesome', name: 'user' }}
+            onChangeText={((txt) => setUserName(txt))}
+            value={username}
+            containerStyle={StyleSheet.formInput}
+            leftIconContainerStyle={styles.formIcon}
+            />
+         <Input
+            placeholder='Password'
+            leftIcon={{ type: 'font-awesome', name: 'key' }}
+            onChangeText={((txt) => setPassword(txt))}
+            value={password}
+            containerStyle={StyleSheet.formInput}
+            leftIconContainerStyle={styles.formIcon}
+         />
+         
+         {/*  */}
+      </View>
+   )
+
+   
+
+
+
+}
+
+export default LoginScreen
